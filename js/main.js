08ff734c5f815c6cd851cf313b5946bb79fc7676
mehediemon007 <mehediemon7@gmail.com>
2022-03-10 13:48:18 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 ;(function($){
+    // Banner
 
+    $(".ecom-banner").owlCarousel({
+        items:1,
+        loop:true,
+        nav:false,
+        dots:true
+    })
+
+    // New Arrivals
+
+    $(".new-products-slider").owlCarousel({
+        items:5,
+        loop:false,
+        margin:16,
+        slideBy:5,
+        nav:true,
+        navText:[$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")],
+        mouseDrag:false
+    })
 
 })(jQuery);
 
